3da50e1a0a38fac1032e920f9f7ce184e7d5a5a3
matsuoka shiho <shihopine10-marie11@docomo.ne.jp>
2017-05-30 18:12:58 +0900

    全コミット

--- first.php
+++ first.php
@@ -1,66 +1,104 @@
 <?php
   //練習問題1
   //文字列連結を用いて、自分の名前をブラウザ上に表示する。
-  $first_name = '';
-  $last_name = '';
+  $first_name = 'shiho';
+  $last_name = 'matsuoka';
+
+ echo"<br>";
 
   //練習問題2
   //以下の計算をそれぞれ行い、ブラウザ上に表示する。
   //8 * 3の計算
-
+  echo 8*3;
   //24 / 8の計算
-
+  echo 24/8;
   //6 + 10の計算
-
+  echo 6+10;
   //6 - 10の計算
-
+  echo 6-10;
   //10 % 7の計算
+  echo 10%7;
+
+  echo"<br>";
 
   //練習問題3
   //下記のプログラムのコメント部分をそれぞれ実行し、ブラウザ上に表示する。
   //※自己代入演算子を用いること
   $var = 2;
   //$varに12を足す
-
+  $var += 12;
   //$varに7で割る
-
+  $var /= 7;
   //$varに5を掛ける
-
+  $var *= 5;
   //$varの3の剰余
+  $var %= 3;
+
+   echo"<br>";
 
   //練習問題4
   //配列内に、伊達、櫻井、伊藤、藤尾を入れて、ブラウザ上にそれぞれを表示せよ。
-  $list = array();
+  $list = array('伊達','櫻井','伊藤','藤尾');
+  echo $list[0];
+  echo $list[1];
+  echo $list[2];
+
+   echo"<br>";
 
   //練習問題5
   //連想配列内に、nameキーに'櫻井'、ageキーに'22'、placeキーに'富山'を入力して、ブラウザ上にそれぞれを表示せよ。
-  $lists = array();
+  $lists = array('name'=>'櫻井','age'=>'22','place'=>'富山');
+  echo $lists['name'];
+  echo $lists['age'];
+  echo $lists['place'];
+
+   echo"<br>";
 
   //課題1
   //西暦の計算
   //以下の配列に2つの西暦a, bがあります。その2つのaからbが何年間か出力しなさい。
   $year = array(1994, 2017);
+   echo $year[1]-$year[0];
+
+    echo"<br>";
 
   //課題2
   //かまくらづくり
   //櫻井がはしゃいでかまくらを作り始めました。どれだけの体積の雪が必要か計算しなさい。
   //そのかまくらは四角形であり、以下の配列に外側の1辺と内側の1辺の長さが入っている。(r1 > r2)
   //下の行のecho文のコメントを外すとimageが出てくるのでこちらも参照するとわかりやすい。
-  //echo '<img src="./assets/img/img1.png">';
+  echo '<img src="./assets/img/img1.png">';
   $size = array(6, 4);
+  echo $size[0]*$size[0]*$size[0]-$size[1]*$size[1]*$size[1];
+
+   echo"<br>";
 
   //課題3
   //行列の計算
   //まず、以下のecho文のコメントをはずしてください。
   //以下の配列内にa = 10, b = -20, c = 12, d = 6がそれぞれ代入されている。
   //この行列を表す配列を用いて、行列の計算を行いなさい。
-  //echo '<img src="./assets/img/matrix1.png">';
+  echo '<img src="./assets/img/matrix1.png">';
   $matrix = array(10, -20, 12, 6);
+  echo $matrix[0]*$matrix[3]-$matrix[1]*$matrix[2];
+
+   echo"<br>";
 
   //課題4
   //自己紹介
   //練習問題5を参考にして、自己紹介をせよ。
   //例) 私は、安久昌和です。出身は石川県で、年齢は22歳、趣味はバスケットボールです。電子・情報工学科に所属しています。
   //必ず、連想配列を用いること
-  
+  $into = array('name'=>'私は、松岡志帆です。　',
+                'place'=>'出身は愛知県で、',
+                'age'=>'年齢は18歳、',
+                'like'=>'好きなことはディズニーです。',
+                'dept'=>'知能デザイン工学科に所属しています。　');
+  echo $into['name'];
+  echo $into['place'];
+  echo $into['age'];
+  echo $into['like'];
+  echo $into['dept'];
+
+
 ?>
